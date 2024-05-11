e6986080968564a359892cfca3410df1d39e2cb9
sunxinxin <1289139305@qq.com>
2024-05-11 21:24:25 +0800

    第一次进行数据的提交。。

---
+++ test01/src/com/msb/student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: sunxx
+ * @Date: 2024/5/11 - 05 - 11 - 21:20
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class student {
+    private String name;
+    private int id;
+}
